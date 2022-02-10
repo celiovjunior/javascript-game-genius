a7116740f0611c5acfa01b266430d27f5a3de598
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-02-10 13:06:15 -0300

    finalizacao e testes

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ const red = document.querySelector('.red');
 
 // função para criar ordem aleatória
 function shuffleOrder() {
-    let colorOrder = Math.floor(math.random() * 4);
+    let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
     clickedOrder = [];
 
     for(let i in order) {
-        let elementColor = createElement(order[i]);
+        let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number(i) + 1)
     }
 };
@@ -42,7 +42,7 @@ function lightColor(element, number) {
 function checkOrder() {
     for(let i in clickedOrder) {
         if(clickedOrder[i] != order[i]) {
-            lose();
+            gameOver();
             break;
         }
     }
@@ -56,9 +56,57 @@ function checkOrder() {
 // função de registro do click do usuário
 function click(color) {
     clickedOrder[clickedOrder.length] = color;
-    elementColor(color).classList.add('selected')
+    createColorElement(color).classList.add('selected')
 
     setTimeout(() => {
-        elementColor(color).classList.remove('selected')
-    })
+        createColorElement(color).classList.remove('selected')
+        checkOrder();
+    }, 250)
+
 }
+
+// função que retorna
+function createColorElement(color) {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if(color == 2) {
+        return yellow
+    } else if(color == 3) {
+        return blue
+    }
+}
+
+// função que vai aumentar o nível do jogo
+function nextLevel() {
+    score++;
+    shuffleOrder()
+}
+
+// função que dispara caso haja game over
+function gameOver() {
+    alert(`Pontuação: ${score}\n Você perdeu o jogo :(\n Clique em OK para reiniciar.`)
+    order = [];
+    clickedOrder = [];
+    playGame();
+}
+
+// play no game
+function playGame() {
+    alert(`Bem vindo ao GENESIS! Iniciando novo jogo!`)
+    score = 0;
+    nextLevel();
+}
+
+green.addEventListener('click', click(0))
+red.addEventListener('click', click(1))
+yellow.addEventListener('click', click(2))
+blue.addEventListener('click', click(3))
+
+green.onclick = () => click(0)
+red.onclick = () => click(1)
+yellow.onclick = () => click(2)
+blue.onclick = () => click(3)
+
+playGame();
