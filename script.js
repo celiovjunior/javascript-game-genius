8d804169809e981b02d1aa516c06d16072417d1d
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-02-10 12:45:23 -0300

    criando funcao de checagem dos clicks do usuario

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 const red = document.querySelector('.red');
 
+// função para criar ordem aleatória
 function shuffleOrder() {
     let colorOrder = Math.floor(math.random() * 4);
     order[order.length] = colorOrder;
@@ -25,6 +26,7 @@ function shuffleOrder() {
     }
 };
 
+// função para acender a cor
 function lightColor(element, number) {
     number = number * 500;
     setTimeout(() => {
@@ -33,5 +35,30 @@ function lightColor(element, number) {
 
     setTimeout(() => {
         element.classList.remove('selected')
+    })
+}
+
+// checagem do botão
+function checkOrder() {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\n Você acertou! Iniciando próximo nível.`)
+        nextLevel();
+    }
+}
+
+// função de registro do click do usuário
+function click(color) {
+    clickedOrder[clickedOrder.length] = color;
+    elementColor(color).classList.add('selected')
+
+    setTimeout(() => {
+        elementColor(color).classList.remove('selected')
     })
 }
